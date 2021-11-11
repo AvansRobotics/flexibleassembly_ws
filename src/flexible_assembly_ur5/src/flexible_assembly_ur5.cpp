a576d2f7b284cbf3e1955dcf5a7d74f75147fd2a
Student <student@avans.nl>
2021-11-11 11:05:20 +0100

    updated setup instructions

--- src/flexible_assembly_ur5/src/flexible_assembly_ur5.cpp
+++ src/flexible_assembly_ur5/src/flexible_assembly_ur5.cpp
@@ -1,87 +1,172 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2013, SRI International
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of SRI International nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Sachin Chitta, Dave Coleman, Mike Lautman */
+
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
-#include <moveit_visual_tools/moveit_visual_tools.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
 
-int main(int argc, char **argv)
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/CollisionObject.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+#include "poses.h"
+//#include <flexible_assembly_ur5/poses.h>
+//#include "poses.cpp"
+
+// The circle constant tau = 2*pi. One tau is one rotation in radians.
+const double tau = 2 * M_PI;
+
+int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "move_cartesian");
+  ros::init(argc, argv, "move_group_interface_tutorial");
   ros::NodeHandle node_handle;
+
+  // ROS spinning must be running for the MoveGroupInterface to get information
+  // about the robot's state. One way to do this is to start an AsyncSpinner
+  // beforehand.
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
+  // BEGIN_TUTORIAL
+  //
+  // Setup
+  // ^^^^^
+  //
+  // MoveIt operatesinclude "poses.h on sets of joints called "planning groups" and stores them in an object called
+  // the `JointModelGroup`. Throughout MoveIt the terms "planning group" and "joint model group"
+  // are used interchangably.
   static const std::string PLANNING_GROUP = "manipulator";
-  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-  const robot_state::JointModelGroup *joint_model_group =
-      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
+  // The :planning_interface:`MoveGroupInterface` class can be easily
+  // setup using just the name of the planning group you would like to control and plan for.
+  moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
+
+  // We will use the :planning_interface:`PlanningSceneInterface`
+  // class to add and remove collision objects in our "virtual world" scene
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+  // Raw pointers are frequently used to refer to the planning group for improved performance.
+  const moveit::core::JointModelGroup* joint_model_group =
+      move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  // Visualizationinclude "poses.h
+  //
+  // The package MoveItVisualTools provides many capabilities for visualizing objects, robots,
+  // and trajectories iprintinfo()n RViz as well as debugging tools such as step-by-step introspection of a script.
   namespace rvt = rviz_visual_tools;
   moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
   visual_tools.deleteAllMarkers();
+
+  // Remote control is an introspection tool that allows users to step through a high level script
+  // via buttons and keyboard shortcuts in RViz
   visual_tools.loadRemoteControl();
 
+  // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
   Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
-  text_pose.translation().z() = 0.8;
+  text_pose.translation().z() = 1.0;
   visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+
+  // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
   visual_tools.trigger();
 
-  ROS_INFO_NAMED("move_cartesian", "Planning frame: %s", move_group.getPlanningFrame().c_str());
-  ROS_INFO_NAMED("move_cartesian", "Available Planning Groups:");
-  std::copy(move_group.getJointModelGroupNames().begin(), move_group.getJointModelGroupNames().end(),
-            std::ostream_iterator<std::string>(std::cout, ", "));
+  // Getting Basic IstartPose
+  // We can print the name of the reference frame for this robot.
+  ROS_INFO_NAMED("tutorial", "Planning frame: %s", move_group_interface.getPlanningFrame().c_str());
 
-  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+  // We can also print the name of the end-effector link for this group.
+  ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group_interface.getEndEffectorLink().c_str());
 
-  //setting waypoints
-  std::vector<geometry_msgs::Pose> waypoints;
-  robot_state::RobotState start_state(*move_group.getCurrentState());
-  geometry_msgs::Pose start_pose;
-  start_pose.orientation.y = sqrt(2) / 2;
-  start_pose.orientation.w = sqrt(2) / 2;
-  start_pose.position.x = 0.3;
-  start_pose.position.y = 0.15;
-  start_pose.position.z = 0.3;
-  waypoints.push_back(start_pose);
+  // We can get a list of all the groups in   easily the robot:
+  ROS_INFO_NAMED("tutorial", "Available Planning Groups:");
+  std::copy(move_group_interface.getJointModelGroupNames().begin(),
+            move_group_interface.getJointModelGroupNames().end(), std::ostream_iterator<std::string>(std::cout, ", "));
 
-  geometry_msgs::Pose target_pose = start_pose;
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-  target_pose.position.y -= 0.15;
-  target_pose.position.z -= 0.15;
-  waypoints.push_back(target_pose); // down right
+  // TODO!:
+  moveit::core::RobotStatePtr current_state = move_group_interface.getCurrentState();
+  //
+  // Next get the current set of joint values for the group.
+  std::vector<double> joint_group_positions;
+  current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
 
-  target_pose.position.y -= 0.15;
-  target_pose.position.z += 0.15;
-  waypoints.push_back(target_pose); // up right
+  std::cout << "joint_group_positions before:" << std::endl;
+  for (double i: joint_group_positions)
+    std::cout << i << std::endl;
 
-  target_pose.orientation.y = 0;
-  target_pose.orientation.w = 1;
-  target_pose.position.y += 0.15;
-  target_pose.position.z += 0.15;
-  waypoints.push_back(target_pose); // up left
+  // Now, let's modify one of the joints, plan to the new joint space goal and visualize the plan.
+  // joint_group_positions[0] = -1.3825;  // -1/6 turn in radians
+  // joint_group_positions[1] = -1.7265;
+  // joint_group_positions[2] = -1.8083;
+  // joint_group_positions[3] = -1.1071;
+  // joint_group_positions[4] = 1.5708;
+  // joint_group_positions[5] = 0.4221;
+  
+  // std::vector<float> joint_poses{-1.3825,-1.7265,-1.8083,-1.1071,1.5708,0.4221};
+  // JointPose startPose("startPose", joint_poses);
+  JointPose startpose("naam"); 
+  startpose.printinfo();
 
-  target_pose.position.y += 0.15;
-  target_pose.position.z -= 0.15;
-  waypoints.push_back(target_pose); // down left
+  joint_group_positions[0] = joint_group_positions[0] + 0.2;
 
-  //planning
-  moveit_msgs::RobotTrajectory trajectory;
-  const double jump_threshold = 0.0;
-  const double eef_step = 0.01;
-  double fraction = move_group.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
-  ROS_INFO_NAMED("move_cartesian", "Visualizing plan through waypoints (%.2f%% acheived)", fraction * 100.0);
+  std::cout << "joint_group_positions after:" << std::endl;
+  for (double i: joint_group_positions)
+    std::cout << i << std::endl;
 
-  // Visualize the plan in RViz
-  visual_tools.deleteAllMarkers();
-  visual_tools.publishText(text_pose, "Waypoints", rvt::WHITE, rvt::XLARGE);
-  visual_tools.publishPath(waypoints, rvt::MAGENTA, rvt::SMALL);
-  for (std::size_t i = 0; i < waypoints.size(); ++i)
-    visual_tools.publishAxisLabeled(waypoints[i], "pt" + std::to_string(i), rvt::MEDIUM);
-  visual_tools.trigger();
-  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue with path execution");
+  move_group_interface.setJointValueTarget(joint_group_positions);
 
-  //path execution
-  move_group.execute(trajectory);
-  //visual_tools.prompt("Press 'next' to exit the program");
+  // We lower the allowed maximum velocity and acceleration to 5% of their maximum.
+  // The default values are 10% (0.1).
+  // Set your preferred defaults in the joint_limits.yaml file of your robot's moveit_config
+  // or set explicit factors in your code if you need your robot to move faster.
+  move_group_interface.setMaxVelocityScalingFactor(0.05);
+  move_group_interface.setMaxAccelerationScalingFactor(0.05);
+
+  bool success = (move_group_interface.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  if(success){
+    move_group_interface.execute(my_plan);
+  }
+  else{
+    std::cout << "unsuccesfull" << std::endl;
+  }
+
+  std::cout << "*********************************Done" << std::endl;
+
 
   ros::shutdown();
   return 0;
