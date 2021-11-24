e0dd7e7a84ca02ff9f9b6d83cdb44f4732ba2b4e
Student <student@avans.nl>
2021-11-24 10:04:35 +0100

    ur5 fix

--- src/flexible_assembly_ur5/src/flexible_assembly_ur5.cpp
+++ src/flexible_assembly_ur5/src/flexible_assembly_ur5.cpp
@@ -44,14 +44,94 @@
 #include <moveit_msgs/CollisionObject.h>
 
 #include <moveit_visual_tools/moveit_visual_tools.h>
-#include "poses.h"
-//#include <flexible_assembly_ur5/poses.h>
-//#include "poses.cpp"
+#include "geometric_shapes/shapes.h"
+#include "geometric_shapes/mesh_operations.h"
+#include "geometric_shapes/shape_operations.h"
 
 // The circle constant tau = 2*pi. One tau is one rotation in radians.
 const double tau = 2 * M_PI;
 
-int main(int argc, char** argv)
+std::vector<double> zeroPose = {0, 0, 0, 0, 0, 0};
+std::vector<double> homePose = {0, -1.57, 0, -1.57, 0, 0};
+
+bool robotIsatPose(std::vector<double> current_joint_positions, std::vector<double> expected_joint_positions)
+{
+  for (int i : current_joint_positions)
+  {
+    if (!abs(current_joint_positions[i] - goal_positions[i]) < 0.005)
+    {
+      std::cout << "Robot is not at the expected pose, please move the robot manually to the expected pose." << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
+bool addCollisionObjects()
+{
+  //add objects to scene
+  moveit_msgs::CollisionObject collision_object;
+  collision_object.header.frame_id = move_group_interface.getPlanningFrame();
+
+  collision_object.id = "table";
+
+  shape_msgs::SolidPrimitive primitive;
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[0] = 10;
+  primitive.dimensions[1] = 10;
+  primitive.dimensions[2] = 10;
+
+  geometry_msgs::Pose box_pose;
+  box_pose.orientation.w = 1.0;
+  box_pose.position.x = 0.0;
+  box_pose.position.y = 0.0;
+  box_pose.position.z = 0.0;
+
+  collision_object.primitives.push_back(primitive);
+  collision_object.primitive_poses.push_back(box_pose);
+  collision_object.operation = collision_object.ADD;
+
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.push_back(collision_object);
+
+  planning_scene_interface.addCollisionObjects(collision_objects);
+
+  //std::Vector3d b(0.001, 0.001, 0.001);
+  // moveit_msgs::CollisionObject co;
+  // co.id = "table";
+  // shapes::Mesh *m = shapes::createMeshFromResource("package://ur3_with_vacuum_tool_moveit_config/meshes/World.STL");
+  // ROS_INFO("World mesh loaded");
+
+  // shape_msgs::Mesh mesh;
+  // shapes::ShapeMsg mesh_msg;
+  // shapes::constructMsgFromShape(m, mesh_msg);
+  // mesh = boost::get<shape_msgs::Mesh>(mesh_msg);
+
+  // //co.meshes.resize(1);
+  // //co.mesh_poses.resize(1);
+  // co.meshes[0] = mesh;
+  // co.header.frame_id = move_group_interface.getPlanningFrame();
+  // co.mesh_poses[0].position.x = 0;
+  // co.mesh_poses[0].position.y = 0;
+  // co.mesh_poses[0].position.z = 0.0;
+  // co.mesh_poses[0].orientation.w = 0.0;
+  // co.mesh_poses[0].orientation.x = 0.0;
+  // co.mesh_poses[0].orientation.y = 0.0;
+  // co.mesh_poses[0].orientation.z = 0.0;
+
+  // co.meshes.push_back(mesh);
+  // co.mesh_poses.push_back(co.mesh_poses[0]);
+  // co.operation = co.ADD;
+  // std::vector<moveit_msgs::CollisionObject> vec;
+  // vec.push_back(co);
+  // ROS_INFO("World added into the world");
+  // planning_scene_interface.addCollisionObjects(vec);
+  // sleep(5.0);
+  ////////////////////////////
+}
+
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "move_group_interface_tutorial");
   ros::NodeHandle node_handle;
@@ -62,90 +142,46 @@ int main(int argc, char** argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  // BEGIN_TUTORIAL
-  //
-  // Setup
-  // ^^^^^
-  //
-  // MoveIt operatesinclude "poses.h on sets of joints called "planning groups" and stores them in an object called
-  // the `JointModelGroup`. Throughout MoveIt the terms "planning group" and "joint model group"
-  // are used interchangably.
   static const std::string PLANNING_GROUP = "manipulator";
-
-  // The :planning_interface:`MoveGroupInterface` class can be easily
-  // setup using just the name of the planning group you would like to control and plan for.
   moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
-
-  // We will use the :planning_interface:`PlanningSceneInterface`
-  // class to add and remove collision objects in our "virtual world" scene
   moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-  // Raw pointers are frequently used to refer to the planning group for improved performance.
-  const moveit::core::JointModelGroup* joint_model_group =
-      move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+  const moveit::core::JointModelGroup *joint_model_group = move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-  // Visualizationinclude "poses.h
-  //
-  // The package MoveItVisualTools provides many capabilities for visualizing objects, robots,
-  // and trajectories iprintinfo()n RViz as well as debugging tools such as step-by-step introspection of a script.
-  namespace rvt = rviz_visual_tools;
-  moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
-  visual_tools.deleteAllMarkers();
-
-  // Remote control is an introspection tool that allows users to step through a high level script
-  // via buttons and keyboard shortcuts in RViz
-  visual_tools.loadRemoteControl();
-
-  // RViz provides many types of markers, in this demo we will use text, cylinders, and spheres
-  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
-  text_pose.translation().z() = 1.0;
-  visual_tools.publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
-
-  // Batch publishing is used to reduce the number of messages being sent to RViz for large visualizations
-  visual_tools.trigger();
-
-  // Getting Basic IstartPose
-  // We can print the name of the reference frame for this robot.
-  ROS_INFO_NAMED("tutorial", "Planning frame: %s", move_group_interface.getPlanningFrame().c_str());
-
-  // We can also print the name of the end-effector link for this group.
-  ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group_interface.getEndEffectorLink().c_str());
-
-  // We can get a list of all the groups in   easily the robot:
-  ROS_INFO_NAMED("tutorial", "Available Planning Groups:");
+  ROS_INFO_NAMED("Flexible_assembly", "Planning frame: %s", move_group_interface.getPlanningFrame().c_str());
+  ROS_INFO_NAMED("Flexible_assembly", "End effector link: %s", move_group_interface.getEndEffectorLink().c_str());
+  ROS_INFO_NAMED("Flexible_assembly", "Available Planning Groups:");
   std::copy(move_group_interface.getJointModelGroupNames().begin(),
             move_group_interface.getJointModelGroupNames().end(), std::ostream_iterator<std::string>(std::cout, ", "));
 
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-  // TODO!:
+
+  /////////////////////////////////////////////////////////////////////////////////
+  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
   moveit::core::RobotStatePtr current_state = move_group_interface.getCurrentState();
-  //
-  // Next get the current set of joint values for the group.
+
+  //get the current set of joint values for the group.
   std::vector<double> joint_group_positions;
   current_state->copyJointGroupPositions(joint_model_group, joint_group_positions);
 
   std::cout << "joint_group_positions before:" << std::endl;
-  for (double i: joint_group_positions)
+  for (double i : joint_group_positions)
     std::cout << i << std::endl;
 
-  // Now, let's modify one of the joints, plan to the new joint space goal and visualize the plan.
-  // joint_group_positions[0] = -1.3825;  // -1/6 turn in radians
-  // joint_group_positions[1] = -1.7265;
-  // joint_group_positions[2] = -1.8083;
-  // joint_group_positions[3] = -1.1071;
-  // joint_group_positions[4] = 1.5708;
-  // joint_group_positions[5] = 0.4221;
-  
+  //Now, let's modify one of the joints, plan to the new joint space goal and visualize the plan.
+  joint_group_positions[0] = -1.3825; // -1/6 turn in radians
+  joint_group_positions[1] = -1.7265;
+  joint_group_positions[2] = -1.8083;
+  joint_group_positions[3] = -1.1071;
+  joint_group_positions[4] = 1.5708;
+  joint_group_positions[5] = 0.4221;
+
   // std::vector<float> joint_poses{-1.3825,-1.7265,-1.8083,-1.1071,1.5708,0.4221};
-  // JointPose startPose("startPose", joint_poses);
-  JointPose startpose("naam"); 
-  startpose.printinfo();
 
   joint_group_positions[0] = joint_group_positions[0] + 0.2;
 
   std::cout << "joint_group_positions after:" << std::endl;
-  for (double i: joint_group_positions)
+  for (double i : joint_group_positions)
     std::cout << i << std::endl;
 
   move_group_interface.setJointValueTarget(joint_group_positions);
@@ -158,15 +194,16 @@ int main(int argc, char** argv)
   move_group_interface.setMaxAccelerationScalingFactor(0.05);
 
   bool success = (move_group_interface.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-  if(success){
+  if (success)
+  {
     move_group_interface.execute(my_plan);
   }
-  else{
+  else
+  {
     std::cout << "unsuccesfull" << std::endl;
   }
 
   std::cout << "*********************************Done" << std::endl;
-
 
   ros::shutdown();
   return 0;
