d10ec31aca30516a7612053641cc2a513365ea53
Student <student@avans.nl>
2021-11-24 14:38:51 +0100

    planning to pose

--- src/flexible_assembly_ur5/src/flexible_assembly_ur5.cpp
+++ src/flexible_assembly_ur5/src/flexible_assembly_ur5.cpp
@@ -43,10 +43,11 @@
 #include <moveit_msgs/AttachedCollisionObject.h>
 #include <moveit_msgs/CollisionObject.h>
 
-#include <moveit_visual_tools/moveit_visual_tools.h>
+//#include <moveit_visual_tools/moveit_visual_tools.h>
 #include "geometric_shapes/shapes.h"
 #include "geometric_shapes/mesh_operations.h"
 #include "geometric_shapes/shape_operations.h"
+#include <visualization_msgs/Marker.h>
 
 // The circle constant tau = 2*pi. One tau is one rotation in radians.
 const double tau = 2 * M_PI;
@@ -54,8 +55,6 @@ const double tau = 2 * M_PI;
 std::vector<double> zeroPose = {0, 0, 0, 0, 0, 0};
 std::vector<double> homePose = {0, -1.57, 0, -1.57, 0, 0};
 std::vector<double> prePickPose = {-1, -1.57, 0, -1.57, 0, 0};
-
-
 
 double roundUp(double value, int decimal_places)
 {
@@ -65,7 +64,7 @@ double roundUp(double value, int decimal_places)
 
 void printJointPositions(std::vector<double> joint_positions)
 {
-    for(int i = 0; i < joint_positions.size(); i++)
+  for (int i = 0; i < joint_positions.size(); i++)
   {
     std::cout << "current joint " << i << " position: " << roundUp(joint_positions[i], 4) << std::endl;
   }
@@ -95,74 +94,111 @@ bool robotIsatPose(std::vector<double> current_joint_positions, std::vector<doub
   return true;
 }
 
+visualization_msgs::Marker getMarker(std::string frame_id, geometry_msgs::Pose marker_pose)
+{
+  visualization_msgs::Marker marker;
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = frame_id;
+  marker.header.stamp = ros::Time::now();
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "basic_shapes";
+  marker.id = 0;
+
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  // Set our initial shape type to be a cube
+  marker.type = visualization_msgs::Marker::SPHERE;
+
+  // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+  marker.action = visualization_msgs::Marker::ADD;
+
+  // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+  marker.pose = marker_pose;
+
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker.scale.x = 0.01;
+  marker.scale.y = 0.01;
+  marker.scale.z = 0.01;
+
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker.color.r = 1.0f;
+  marker.color.g = 0.0f;
+  marker.color.b = 0.0f;
+  marker.color.a = 1.0;
+
+  marker.lifetime = ros::Duration(15);
+
+  return marker;
+}
 std::vector<moveit_msgs::CollisionObject> getCollisionObjects(std::string frame_id)
 {
-  //add objects to scene
-  moveit_msgs::CollisionObject collision_object;
-  collision_object.header.frame_id = frame_id; //ove_group_interface.getPlanningFrame();
-
-  collision_object.id = "table";
-
-  shape_msgs::SolidPrimitive primitive;
-  primitive.type = primitive.BOX;
-  primitive.dimensions.resize(3);
-  primitive.dimensions[0] = 10;
-  primitive.dimensions[1] = 10;
-  primitive.dimensions[2] = 10;
-
-  geometry_msgs::Pose box_pose;
-  box_pose.orientation.w = 1.0;
-  box_pose.position.x = 0.0;
-  box_pose.position.y = 0.0;
-  box_pose.position.z = 0.0;
-
-  collision_object.primitives.push_back(primitive);
-  collision_object.primitive_poses.push_back(box_pose);
-  collision_object.operation = collision_object.ADD;
-
   std::vector<moveit_msgs::CollisionObject> collision_objects;
-  collision_objects.push_back(collision_object);
+  // //SHAPE OBJECT
+  // //add objects to scene
+  // moveit_msgs::CollisionObject collision_object;
+  // collision_object.header.frame_id = frame_id; //move_group_interface.getPlanningFrame();
+  // collision_object.id = "table";
+
+  // shape_msgs::SolidPrimitive primitive;
+  // primitive.type = primitive.BOX;
+  // primitive.dimensions.resize(3);
+  // primitive.dimensions[0] = 0.10;
+  // primitive.dimensions[1] = 0.10;
+  // primitive.dimensions[2] = 0.10;
+
+  // geometry_msgs::Pose box_pose;
+  // box_pose.orientation.w = 1.0;
+  // box_pose.position.x = 0.15;
+  // box_pose.position.y = 0.15;
+  // box_pose.position.z = 0.2;
+
+  // collision_object.primitives.push_back(primitive);
+  // collision_object.primitive_poses.push_back(box_pose);
+  // collision_object.operation = collision_object.ADD;
+
+  // collision_objects.push_back(collision_object);
+
+  //WORLD MESH OBJECT
+
+  moveit_msgs::CollisionObject worldObj;
+  shape_msgs::Mesh world_mesh;
+  shapes::ShapeMsg world_mesh_msg;
+  shapes::Mesh *m = shapes::createMeshFromResource("package://ur3_with_vacuum_tool_moveit_config/meshes/World.STL");
+  shapes::constructMsgFromShape(m, world_mesh_msg);
+  world_mesh = boost::get<shape_msgs::Mesh>(world_mesh_msg);
+
+  worldObj.meshes.resize(1);
+  worldObj.meshes[0] = world_mesh;
+  worldObj.mesh_poses.resize(1);
+  worldObj.header.frame_id = frame_id;
+  worldObj.id = "world";
+  worldObj.mesh_poses[0].position.x = -0.20;
+  worldObj.mesh_poses[0].position.y = -0.15;
+  worldObj.mesh_poses[0].position.z = -0.025;
+  worldObj.mesh_poses[0].orientation.w = 0.0;
+  worldObj.mesh_poses[0].orientation.x = 0.0;
+  worldObj.mesh_poses[0].orientation.y = 0.0;
+  worldObj.mesh_poses[0].orientation.z = 0.0;
+
+  worldObj.meshes.push_back(world_mesh);
+  worldObj.mesh_poses.push_back(worldObj.mesh_poses[0]);
+  worldObj.operation = worldObj.ADD;
+  // worldObj.meshes.push_back(mesh);
+  // worldObj.mesh_poses.push_back(worldObj.mesh_poses[0]);
+  // worldObj.operation = worldObj.ADD;
+  collision_objects.push_back(worldObj);
 
   return collision_objects;
-
-  //std::Vector3d b(0.001, 0.001, 0.001);
-  // moveit_msgs::CollisionObject co;
-  // co.id = "table";
-  // shapes::Mesh *m = shapes::createMeshFromResource("package://ur3_with_vacuum_tool_moveit_config/meshes/World.STL");
-  // ROS_INFO("World mesh loaded");
-
-  // shape_msgs::Mesh mesh;
-  // shapes::ShapeMsg mesh_msg;
-  // shapes::constructMsgFromShape(m, mesh_msg);
-  // mesh = boost::get<shape_msgs::Mesh>(mesh_msg);
-
-  // //co.meshes.resize(1);
-  // //co.mesh_poses.resize(1);
-  // co.meshes[0] = mesh;
-  // co.header.frame_id = move_group_interface.getPlanningFrame();
-  // co.mesh_poses[0].position.x = 0;
-  // co.mesh_poses[0].position.y = 0;
-  // co.mesh_poses[0].position.z = 0.0;
-  // co.mesh_poses[0].orientation.w = 0.0;
-  // co.mesh_poses[0].orientation.x = 0.0;
-  // co.mesh_poses[0].orientation.y = 0.0;
-  // co.mesh_poses[0].orientation.z = 0.0;
-
-  // co.meshes.push_back(mesh);
-  // co.mesh_poses.push_back(co.mesh_poses[0]);
-  // co.operation = co.ADD;
-  // std::vector<moveit_msgs::CollisionObject> vec;
-  // vec.push_back(co);
-  // ROS_INFO("World added into the world");
-  // planning_scene_interface.addCollisionObjects(vec);
-  // sleep(5.0);
-  ////////////////////////////
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "flexible_assembly");
   ros::NodeHandle node_handle;
+
+  //pub sub
+  ros::Publisher marker_pub = node_handle.advertise<visualization_msgs::Marker>("visualization_marker", 0);
 
   // ROS spinning must be running for the MoveGroupInterface to get information
   // about the robot's state. One way to do this is to start an AsyncSpinner
@@ -181,18 +217,24 @@ int main(int argc, char **argv)
   ROS_INFO_NAMED("Flexible_assembly", "Available Planning Groups:");
   std::copy(move_group_interface.getJointModelGroupNames().begin(),
             move_group_interface.getJointModelGroupNames().end(), std::ostream_iterator<std::string>(std::cout, ", "));
-  std::cout << std::endl << std::endl;
+  std::cout << std::endl
+            << std::endl;
 
   /////////////////////////////////////////////////////////////////////////////////
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
   moveit::core::RobotStatePtr current_state = move_group_interface.getCurrentState();
   //set moveit params
+  move_group_interface.setPlannerId("OMPL");
+  move_group_interface.allowReplanning(true);
   move_group_interface.setPlanningTime(5.0);
+  move_group_interface.setNumPlanningAttempts(10);
   move_group_interface.setMaxVelocityScalingFactor(1.0);
   move_group_interface.setMaxAccelerationScalingFactor(1.0);
 
-  //planning_scene_interface.addCollisionObjects(getCollisionObjects(move_group_interface.getPlanningFrame()));
-  sleep(3);
+  //add collision objects to scene
+  std::vector<moveit_msgs::CollisionObject> collision_objects = getCollisionObjects(move_group_interface.getPlanningFrame());
+  planning_scene_interface.addCollisionObjects(collision_objects);
+  sleep(5.0);
 
   //get current joint positions
   std::vector<double> current_joint_positions;
@@ -205,7 +247,7 @@ int main(int argc, char **argv)
     ros::shutdown();
   }
 
-  //set jointvalues to prePickPose
+  //prePickPose: set jointvalues, plan, execute
   move_group_interface.setJointValueTarget(prePickPose);
   if (!move_group_interface.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
   {
@@ -213,12 +255,49 @@ int main(int argc, char **argv)
   }
   else
   {
-    if(!move_group_interface.execute(my_plan)){
+    if (!move_group_interface.execute(my_plan))
+    {
       std::cout << " execute plan not finished";
     }
   }
 
-  //set jointvalues to homePoise
+  move_group_interface.setStartState(*move_group_interface.getCurrentState());
+  geometry_msgs::Pose target_pose1; 
+  target_pose1.orientation.w = 1.0;
+  target_pose1.position.x = 0.3;
+  target_pose1.position.y = 0.3;
+  target_pose1.position.z = 0.4;
+  move_group_interface.setPoseTarget(target_pose1);
+
+  //SHOW MARKER AT TARGET POSE
+  visualization_msgs::Marker marker = getMarker(move_group_interface.getPlanningFrame(), target_pose1);
+  marker_pub.publish(marker);
+  //
+
+  moveit::planning_interface::MoveItErrorCode ret = move_group_interface.plan(my_plan);
+  std::cout << "ret: " << ret << std::endl;
+  //int32 SUCCESS=1
+  // int32 FAILURE=99999
+  // int32 PLANNING_FAILED=-1
+  // int32 INVALID_MOTION_PLAN=-2
+  // int32 MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE=-3
+  // int32 CONTROL_FAILED=-4
+  // int32 UNABLE_TO_AQUIRE_SENSOR_DATA=-5
+  // int32 TIMED_OUT=-6
+  // int32 PREEMPTED=-7
+
+  //if (!move_group_interface.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
+  if (ret != moveit::planning_interface::MoveItErrorCode::SUCCESS)
+  {
+    std::cout << "planning unsuccesfull" << std::endl;
+  }
+  else
+  {
+    ROS_INFO("PLANNED< EXECUTION POSSIBLE");
+    //move_group_interface.execute(my_plan);
+  }
+
+  //homePose: set jointvalues, plan, execute
   move_group_interface.setJointValueTarget(homePose);
   if (!move_group_interface.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
   {
@@ -228,6 +307,14 @@ int main(int argc, char **argv)
   {
     move_group_interface.execute(my_plan);
   }
+
+  std::vector<std::string> object_ids;
+  for (auto collision_object : collision_objects)
+  {
+    object_ids.push_back(collision_object.id);
+  }
+  planning_scene_interface.removeCollisionObjects(object_ids);
+  sleep(3.0);
 
   ros::shutdown();
   return 0;
